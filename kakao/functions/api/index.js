21c0d3d6065c40bbc89e455a2b423a72a5861a32
sihyeong <tlgud2325@naver.com>
2021-11-24 23:32:59 +0900

    [FIX] init server

--- kakao/functions/api/index.js
+++ kakao/functions/api/index.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 
 //라우팅 : routes 폴더로 관리
-app.use("/", require("./routes"));
+app.use("/", require("../api/routes"));
 
 //route 폴더에 우리가 지정할 경로로 요청이 올 경우,
 //잘못된 경로로 요청이 들어왔다는 메시지를 클라이언트에 보냄
